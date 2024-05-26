894c3d9937150516af7c9c33fbc220992a6e3e23
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-26 12:34:24 -0400

    5/26/2024

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        String inputFile = "data/shakespeare.txt";
+        try {
+            //reade the file - next to put words into code, we need the Map bc key is unique, to avoid repeated words
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+
+            Map<String, Short> wordsToCode = new HashMap<>();
+            short codeCounter = 0;
+            String line = reader.readLine();
+
+            while (line != null) {
+                String[] words = line.split("(?<=\\s)|(?=\\s)");
+                for (String w : words) {
+                    Short existingCode = wordsToCode.get(w);
+                    if(existingCode==null){
+                        wordsToCode.put(w,codeCounter);
+                        codeCounter++;
+                        if(codeCounter==Short.MAX_VALUE){
+                            throw
+                        }
+                    }
+                    System.out.println(w);
+                }
+                System.out.println();
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            //wrote myself
+            System.err.println("Sorry we couldn't find a file " + inputFile);
+        } catch (IOException e) {
+            System.err.println("Sorry, error occured during reading " + inputFile + " error " + e);
+        }
+    }
+}
